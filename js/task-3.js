32eec3f4f6029ae170388ee9d8c909bb009601d4
vladoss2899 <vladoss2899@gmail.com>
2023-11-08 18:10:35 +0100

    a

--- js/task-3.js
+++ js/task-3.js
@@ -2,13 +2,11 @@
 
 function filterArray(numbers, value) {
   let filteredArray = [];
-
   for (let i = 0; i < numbers.length; i++) {
     if (numbers[i] > value) {
       filteredArray.push(numbers[i]);
     }
   }
-
   return filteredArray;
 }
 
